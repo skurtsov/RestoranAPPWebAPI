84739c0a25d0a6f2fb6e50a4aee6626abbe94320
skurtsov <skurtsov@gmail.com>
2023-02-24 12:10:37 +0100

    test

--- myapi/test.py
+++ myapi/test.py
@@ -1,13 +1,16 @@
 import psycopg2
 
-try:
-        conn = psycopg2.connect(dbname='users', user='myuser', password='123456', host='localhost')
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM my_users')
-        all_items = cursor.fetchall()
-        print(all_items)
-	#return HttpResponse(all_items)
-        cursor.close()
-        conn.close()
-except:
-        print('Can`t establish connection to database')
+restoran = "greek"
+stolik = 55
+zakaz = "miasa"
+# пытаемся подключиться к базе данных
+conn = psycopg2.connect(dbname='restoran', user='myuser', password='S53em4e10', host='localhost')
+# получение объекта курсора
+cursor = conn.cursor()
+# Получаем список всех пользователей
+sql = f"INSERT INTO orders_{restoran}(stolik,zakaz) VALUES({stolik},'{zakaz}');INSERT INTO history_{restoran}(stolik,zakaz) VALUES({stolik},'{zakaz}')"
+cursor.execute(sql)
+conn.commit()
+print('ok')
+cursor.close()  # закрываем курсор
+conn.close()  # закрываем соединение
