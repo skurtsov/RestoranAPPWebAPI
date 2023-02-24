97e430e69baad25fbedec2f9392c995cf40aa211
skurtsov <skurtsov@gmail.com>
2023-02-24 12:02:56 +0100

    Add order function

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapi.views import getsku
+from myapi.views import getsku,norder
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/getsku/',getsku),
+    path('api/norder/',norder),
 ]
