9b3ab7dded6eea6d375e212827ec82d497c0e593
skurtsov <skurtsov@gmail.com>
2023-03-02 14:00:17 +0100

    invalid json fix

--- myapi/views.py
+++ myapi/views.py
@@ -186,7 +186,7 @@ def getuser(request):
         # Получаем список всех пользователей
         cursor.execute(f"SELECT restoran,token FROM users WHERE username='{user}' AND PASSWORD='{password}'")
         all_items = cursor.fetchall()
-        response='{"restoran":'+all_items[0][0]+',"token":'+all_items[0][1]+'}'
+        response='{"restoran":"'+all_items[0][0]+'","token":"'+all_items[0][1]+'"}'
         return HttpResponse(response)
 
         cursor.close()  # закрываем курсор
