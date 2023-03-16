d2a38c2f897b9a6893784174ded204cca0c2007e
skurtsov <skurtsov@gmail.com>
2023-03-16 11:54:09 +0100

    media content by url

--- myproject/settings.py
+++ myproject/settings.py
@@ -134,3 +134,5 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
--- myproject/urls.py
+++ myproject/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from myapi.views import getsku, norder, getorders, deleteall, getuser, deleteid, redactid, newuser, getordersweb, \
     addform, add
 
@@ -31,4 +33,4 @@ urlpatterns = [
     path('api/newuser/', newuser),
     path('manager/add/', add),
     path("manager/addform/", addform, name="addform"),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
