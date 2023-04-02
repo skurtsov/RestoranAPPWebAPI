7bc8a04248ecadb2337d21a4cb7e4d109eb16cf8
skurtsov <skurtsov@gmail.com>
2023-04-02 13:26:02 +0200

    getskuid test 2

--- myapi/views.py
+++ myapi/views.py
@@ -283,29 +283,27 @@ def addform(request):
     return render(request, 'main/addform.html', {'form': form, 'test':'parapa'})
 def getskuid(request):
     restoran = request.GET.get('restoran')
-    id= request.GET.get('restoran')
-    # пытаемся подключиться к базе данных
-    conn = psycopg2.connect(dbname='restoran', user='myuser', password='S53em4e10', host='localhost')
-    # получение объекта курсора
-    cursor = conn.cursor()
-    # Получаем список всех пользователей
-    cursor.execute('SELECT * FROM sku_' + restoran +'WHERE id='+id)
-    all_items = cursor.fetchall()
-    i = 0
-    response = ""
-    # Формируем Жсон
-    response += '{"card_state":['
-    for x in all_items:
-        response += json.dumps({"image": all_items[i][0], "name": all_items[i][1], "name_en": all_items[i][2],
-        "name_fr": all_items[i][3], "desc": all_items[i][4], "desc_en": all_items[i][5],
-        "desc_fr": all_items[i][6], "price": str(all_items[i][11]),
-        "id": str(all_items[i][13])})
-             if (i != len(all_items) - 1):
-                  response += ','
-                  i += 1
+    id = request.GET.get('restoran')
+    try:
+        # пытаемся подключиться к базе данных
+        conn = psycopg2.connect(dbname='restoran', user='myuser', password='S53em4e10', host='localhost')
+        # получение объекта курсора
+        cursor = conn.cursor()
+        # Получаем список всех пользователей
+        cursor.execute('SELECT * FROM sku_' + restoran+' WHERE id='+id )
+        all_items = cursor.fetchall()
 
-    response += '],"orders":[0],"orders_loc":[0],"ordersServer":[]}'
-    return HttpResponse(response)
+
+        return HttpResponse(all_items)
+
+        cursor.close()  # закрываем курсор
+        conn.close()  # закрываем соединение
+
+    except:
+        # в случае сбоя подключения будет выведено сообщение в STDOUT
+        print('Can`t establish connection to database')
+
+
 def add(request):
     name = request.POST['ff']
     return  HttpResponse(name)
