e9f737f23124a7544f2243c0329ac71afdf66f9c
skurtsov <skurtsov@gmail.com>
2023-04-02 23:52:18 +0200

    file no obligatory test

--- myapi/views.py
+++ myapi/views.py
@@ -317,6 +317,7 @@ def redactbyid(request):
                 desc_cat = request.POST['desc_cat']
                 price = request.POST['price']
                 cat = request.POST['category']
+                link = "empty"
                 link = f"https://reactive-cafe.com/media/{restoran}/{file.name}"
                 sql = f"UPDATE sku_{restoran} SET image ='{link}' WHERE id={id}"
                 #sql = f"INSERT INTO sku_{restoran}(image,name,name_en,name_fr,name_de,name_cat,descr,descr_en,descr_fr,descr_de,descr_cat,price,cat,isactive ) VALUES('{link}','{name}','{name_en}','{name_en}','{name_en}','{name_en}','{desc}','{desc_en}','{desc_en}','{desc_en}','{desc_en}',{price},'{cat}',TRUE)"
