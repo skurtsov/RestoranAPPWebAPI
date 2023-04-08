f0302b7e311619ba969e2e30a21b00c6728c9387
skurtsov <skurtsov@gmail.com>
2023-04-08 21:37:06 +0200

    table frm json dev 7

--- myapi/tests.py
+++ myapi/tests.py
@@ -1,22 +1,34 @@
-from django.http import HttpResponse
-import psycopg2
-import os
-
-restoran = 'greek'
-id = 1
+restoran = "greek"
 try:
     # пытаемся подключиться к базе данных
     conn = psycopg2.connect(dbname='restoran', user='myuser', password='S53em4e10', host='localhost')
     # получение объекта курсора
     cursor = conn.cursor()
     # Получаем список всех пользователей
-    cursor.execute(f'SELECT * FROM sku_{restoran} WHERE id={id}')
+    cursor.execute('SELECT * FROM sku_' + restoran + ' ORDER BY id')
     all_items = cursor.fetchall()
+    i = 0
+    response = ""
+    # Формируем Жсон
+    response += '{"card_state":['
+    for x in all_items:
+        response += json.dumps({"image": all_items[i][0], "name": all_items[i][1], "name_en": all_items[i][2],
+                                "name_fr": all_items[i][3], "name_cat": all_items[i][5], "desc": all_items[i][6],
+                                "desc_en": all_items[i][5],
+                                "desc_fr": all_items[i][6], "desc_cat": all_items[i][10],
+                                "price": str(all_items[i][11]),
+                                "id": str(all_items[i][13])})
+        if (i != len(all_items) - 1):
+            response += ','
+            i += 1
+
+    response += ']}'
+    y = json.dumps(response)
+    print(y)
 
     cursor.close()  # закрываем курсор
     conn.close()  # закрываем соединение
-    print(all_items)
 
 except:
     # в случае сбоя подключения будет выведено сообщение в STDOUT
-    print(f'SELECT * FROM sku_{restoran} WHERE id={id}')
+    print('Can`t establish connection to database')
