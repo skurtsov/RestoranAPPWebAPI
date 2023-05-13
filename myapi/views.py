9f04c7a96f5bd9cd90a93725eb81a7d5487233c6
skurtsov <skurtsov@gmail.com>
2023-05-13 20:02:57 +0200

    auth form bugfix kostil

--- myapi/views.py
+++ myapi/views.py
@@ -495,5 +495,5 @@ def signinform(request):
 #         print('Can`t establish connection to database')
 def signin(request):
     uname = request.POST.get('uname')
-    passw = str(hashlib.md5(request.POST.get('passw')))
-    return HttpResponse(str(uname)+passw)
+    passw = request.POST.get('passw')
+    return HttpResponse(str(uname)+str(passw))
