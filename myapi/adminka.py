4d1861fcfe33c063a519b662de79772d3f9f6ab7
skurtsov <skurtsov@gmail.com>
2023-05-29 11:23:47 +0200

    admin init

---
+++ myapi/adminka.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+import psycopg2
+import json
+import hashlib
+from django.shortcuts import render,redirect
+from django.template import loader
+from .forms import ResumeForm
+from .forms import DrinkForm
+import os
+
+# Create your views here.
+# visitor menu
+def testadmin():
+    return HttpResponse("Adminka works")
