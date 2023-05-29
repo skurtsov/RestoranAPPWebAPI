7e97a611efc5c537d467a32a7f77f073403d1cb2
skurtsov <skurtsov@gmail.com>
2023-05-29 11:51:16 +0200

    auth function prototype

--- myapi/adminka.py
+++ myapi/adminka.py
@@ -10,5 +10,21 @@ import os
 
 # Create your views here.
 # visitor menu
-def testadmin(request):
-    return HttpResponse("Adminka works")
+def auth(request):
+    username="marta"
+    try:
+        # пытаемся подключиться к базе данных
+        conn = psycopg2.connect(dbname='restoran', user='myuser', password='S53em4e10', host='localhost')
+        # получение объекта курсора
+        cursor = conn.cursor()
+        # Получаем список всех пользователей
+        cursor.execute(f"SELECT * FROM users WHERE username='{username}'")
+        user_data = cursor.fetchall()
+        return HttpResponse(user_data)
+
+        cursor.close()  # закрываем курсор
+        conn.close()  # закрываем соединение
+
+    except:
+        # в случае сбоя подключения будет выведено сообщение в STDOUT
+        return HttpResponse('User not found')
