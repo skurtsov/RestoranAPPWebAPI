1c397ecec14e1022acf7d63941bdccffc4925e51
skurtsov <skurtsov@gmail.com>
2023-07-14 14:16:06 +0200

    admin panel catch a bug 2

--- myapi/views.py
+++ myapi/views.py
@@ -509,40 +509,35 @@ def profile(request):
 
         cursor.close()  # закрываем курсор
         conn.close()  # закрываем соединение
-        try:
-            # пытаемся подключиться к базе данных
-            conn = psycopg2.connect(dbname='restoran', user='myuser', password='S53em4e10', host='localhost')
-            # получение объекта курсора
-            cursor = conn.cursor()
-            # Получаем список всех пользователей
-            cursor.execute('SELECT * FROM sku_' + user_data + ' ORDER BY id')
-            all_items = cursor.fetchall()
-            i = 0
-            response = ""
-            # Формируем Жсон
-            response += '['
-            for x in all_items:
-                response += json.dumps({"image": all_items[i][0], "name": all_items[i][1], "name_en": all_items[i][2],
-                                        "name_fr": all_items[i][3], "name_cat": all_items[i][5],
-                                        "desc": all_items[i][6],
-                                        "desc_en": all_items[i][5],
-                                        "desc_fr": all_items[i][6], "desc_cat": all_items[i][10],
-                                        "price": str(all_items[i][11]),
-                                        "id": str(all_items[i][13])})
-                if (i != len(all_items) - 1):
-                    response += ','
-                    i += 1
+        # пытаемся подключиться к базе данных
+        conn = psycopg2.connect(dbname='restoran', user='myuser', password='S53em4e10', host='localhost')
+        # получение объекта курсора
+        cursor = conn.cursor()
+        # Получаем список всех пользователей
+        cursor.execute('SELECT * FROM sku_' + user_data + ' ORDER BY id')
+        all_items = cursor.fetchall()
+        i = 0
+        response = ""
+        # Формируем Жсон
+        response += '['
+        for x in all_items:
+            response += json.dumps({"image": all_items[i][0], "name": all_items[i][1], "name_en": all_items[i][2],
+                                    "name_fr": all_items[i][3], "name_cat": all_items[i][5],
+                                    "desc": all_items[i][6],
+                                    "desc_en": all_items[i][5],
+                                    "desc_fr": all_items[i][6], "desc_cat": all_items[i][10],
+                                    "price": str(all_items[i][11]),
+                                    "id": str(all_items[i][13])})
+            if (i != len(all_items) - 1):
+                response += ','
+                i += 1
 
-            response += ']'
-            y = json.loads(response)
-            cursor.close()  # закрываем курсор
-            conn.close()  # закрываем соединение
-            return render(request, 'main/table.html', {"myjson": y,
-                                                       "restoran": user_data})  # ^=     ^c ^g        ^a     ^a        ^a   ^e        ^l         ^b                                                                                                                                                                                                           cursor.execute('SELECT * FROM sku_' + restoran + ' ORDER BY id')                                                                                                                                                                                                               all_items = cursor.fetchall()                                                                                                                                                                                                                                                  i = 0                                                                                                                                                                                                                                                                          response = ""                                                                                                                                                                                                                                                                  #      ^`     ^` ^c      ^v ^a                                                                                                                                                                                                                                                 response += '['                                                                                                                                                                                                                                                                for x in all_items:                                                                                                                                                                                                                                                                response += json.dumps({"image": all_items[i][0], "name": all_items[i][1], "name_en": all_items[i][2],                                                                                                                                                                                                 "name_fr": all_items[i][3], "name_cat": all_items[i][5], "desc": all_items[i][6],                                                                                                                                                                                              "desc_en": all_items[i][5],                                                                                                                                                                                                                                                    "desc_fr": all_items[i][6], "desc_cat": all_items[i][10],                                                                                                                                                                                                                      "price": str(all_items[i][11]),                                                                                                                                                                                                                                                "id": str(all_items[i][13])})                                                                                                                                                                                                                          if (i != len(all_items) - 1):                                                                                                                                                                                                                                                      response += ','                                                                                                                                                                                                                                                                i += 1                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                response += ']'                                                                                                                                                                                                                                                                y =json.loads(response)                                                                                                                                                                                                                                                        print(y[0]["name"])                                                                                                                                                                                                                                                            cursor.close()  #        ^` ^k            ^c ^` ^a   ^`                                                                                                                                                                                                                        conn.close()  #        ^` ^k          ^a
-        except:
-            # в случае сбоя подключения будет выведено сообщение в STDOUT
-            return HttpResponse('SELECT * FROM sku_' + user_data + ' ORDER BY id')
-
+        response += ']'
+        y = json.loads(response)
+        cursor.close()  # закрываем курсор
+        conn.close()  # закрываем соединение
+        return render(request, 'main/table.html', {"myjson": y,
+                                                   "restoran": user_data})  # ^=     ^c ^g        ^a     ^a        ^a   ^e        ^l         ^b
 
     except:
         # в случае сбоя подключения будет выведено сообщение в STDOUT
