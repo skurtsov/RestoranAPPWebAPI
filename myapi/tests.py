cf50fe867eafacce487be9deaf1a23a6dcdb1fea
skurtsov <skurtsov@gmail.com>
2023-07-14 14:31:49 +0200

    user_info to test file

--- myapi/tests.py
+++ myapi/tests.py
@@ -1,38 +1,17 @@
-from django.http import HttpResponse
-import psycopg2
-import json
-import hashlib
-from django.shortcuts import render
-from django.template import loader
-from .forms import ResumeForm
-import os
+token = 'marta'
+    try:
+        # пытаемся подключиться к базе данных
+        conn = psycopg2.connect(dbname='restoran', user='myuser', password='S53em4e10', host='localhost')
+        # получение объекта курсора
+        cursor = conn.cursor()
+        # Получаем список всех пользователей
+        cursor.execute(f"SELECT restoran FROM users WHERE token='{token}'")
+        user_data = cursor.fetchall()
+        #return HttpResponse(f'SELECT * FROM sku_{user_data} ORDER BY id')
+        print(user_data)
+        cursor.close()  # закрываем курсор
+        conn.close()  # закрываем соединение
 
-restoran = "greek"
-# пытаемся подключиться к базе данных
-conn = psycopg2.connect(dbname='restoran', user='myuser', password='S53em4e10', host='localhost')
-# получение объекта курсора
-cursor = conn.cursor()
-# Получаем список всех пользователей
-cursor.execute('SELECT * FROM sku_' + restoran + ' ORDER BY id')
-all_items = cursor.fetchall()
-i = 0
-response = ""
-# Формируем Жсон
-response += '['
-for x in all_items:
-    response += json.dumps({"image": all_items[i][0], "name": all_items[i][1], "name_en": all_items[i][2],
-                            "name_fr": all_items[i][3], "name_cat": all_items[i][5], "desc": all_items[i][6],
-                            "desc_en": all_items[i][5],
-                            "desc_fr": all_items[i][6], "desc_cat": all_items[i][10],
-                            "price": str(all_items[i][11]),
-                            "id": str(all_items[i][13])})
-    if (i != len(all_items) - 1):
-        response += ','
-        i += 1
-
-response += ']'
-y = json.loads(response)
-print(y[0]["name"])
-
-cursor.close()  # закрываем курсор
-conn.close()  # закрываем соединение
+    except:
+        # в случае сбоя подключения будет выведено сообщение в STDOUT
+        return HttpResponse('User not found')
