30cfe64b4995dc35b8415d48f7785eb5472d6ff5
skurtsov <skurtsov@gmail.com>
2023-07-14 14:19:06 +0200

    debug funct 2

--- myapi/views.py
+++ myapi/views.py
@@ -506,16 +506,11 @@ def profile(request):
         cursor.execute(f"SELECT restoran FROM users WHERE token='{token}'")
         user_data = cursor.fetchall()
 
-
+        return HttpResponse('SELECT * FROM sku_' + user_data + ' ORDER BY id')
         cursor.close()  # закрываем курсор
         conn.close()  # закрываем соединение
-        # пытаемся подключиться к базе данных
-        conn = psycopg2.connect(dbname='restoran', user='myuser', password='S53em4e10', host='localhost')
-        # получение объекта курсора
-        cursor = conn.cursor()
-        # Получаем список всех пользователей
-        #cursor.execute('SELECT * FROM sku_' + user_data + ' ORDER BY id')
-        return HttpResponse('SELECT * FROM sku_' + user_data + ' ORDER BY id')
+
+
         # all_items = cursor.fetchall()
         # i = 0
         # response = ""
