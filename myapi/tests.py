63dac3dab67faddb58c5ab54b5a2118d1f9b4df8
skurtsov <skurtsov@gmail.com>
2023-07-19 11:45:31 +0200

    redirect idea implement

--- myapi/tests.py
+++ myapi/tests.py
@@ -1,17 +1,14 @@
 token = 'marta'
-    try:
-        # пытаемся подключиться к базе данных
-        conn = psycopg2.connect(dbname='restoran', user='myuser', password='S53em4e10', host='localhost')
-        # получение объекта курсора
-        cursor = conn.cursor()
-        # Получаем список всех пользователей
-        cursor.execute(f"SELECT restoran FROM users WHERE token='{token}'")
-        user_data = cursor.fetchall()
-        #return HttpResponse(f'SELECT * FROM sku_{user_data} ORDER BY id')
-        print(user_data)
-        cursor.close()  # закрываем курсор
-        conn.close()  # закрываем соединение
 
-    except:
-        # в случае сбоя подключения будет выведено сообщение в STDOUT
-        return HttpResponse('User not found')
+# пытаемся подключиться к базе данных
+conn = psycopg2.connect(dbname='restoran', user='myuser', password='S53em4e10', host='localhost')
+# получение объекта курсора
+cursor = conn.cursor()
+# Получаем список всех пользователей
+cursor.execute(f"SELECT restoran FROM users WHERE token='{token}'")
+user_data = cursor.fetchall()
+# return HttpResponse(f'SELECT * FROM sku_{user_data} ORDER BY id')
+str = str(user_data)
+print(str[3:len(str)-4])
+cursor.close()  # закрываем курсор
+conn.close()  # закрываем соединение
--- myapi/views.py
+++ myapi/views.py
@@ -505,8 +505,9 @@ def profile(request):
         # Получаем список всех пользователей
         cursor.execute(f"SELECT restoran FROM users WHERE token='{token}'")
         user_data = cursor.fetchall()
-       #return HttpResponse(f'SELECT * FROM sku_{user_data} ORDER BY id')
-        return HttpResponse(user_data)
+        str = str(user_data)
+        req= str[3:len(str) - 4]
+        return redirect('https://reactive-cafe.com/api/getskutable/?restoran='+req)
         cursor.close()  # закрываем курсор
         conn.close()  # закрываем соединение
 
